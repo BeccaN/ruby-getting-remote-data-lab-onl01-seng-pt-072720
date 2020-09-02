72e38de2d13b7549b9a6b2fd69053ae66ea7de96
Rebecca Congi <rncongi@gmail.com>
2020-09-02 18:39:06 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
 # Write your code here
+require 'open-uri'
+
+
+
+class GetRequester
+  
+  attr_reader :url
+  
+  def initialize(url)
+    @url = url 
+  end   
+  
+  def get_response_body
+      
+  end 
+  
+  
+end   
